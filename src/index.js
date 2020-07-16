5763ce040f9fe9c85b10e53183b72c252e80abb5
DannyCoop <cooperdaniel5141@gmail.com>
2020-07-16 15:59:33 -0400

    Added the add toy functionn

--- src/index.js
+++ src/index.js
@@ -5,11 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const toyFormContainer = document.querySelector(".container");
   const toyCollection = document.querySelector("#toy-collection");
   const toyUrl = "http://localhost:3000/toys";
+  const newToyForm = document.querySelector(".add-toy-form")
+  
 
   const fetchAllToys = () => {
     fetch(toyUrl)
     .then(function(response) {return response.json()})
     .then(function(toys) {toys.forEach(toy => createToy(toy))})
+  }
+
+  const postToy = (toy) =>{
+    fetch(toyUrl, {
+      method : 'POST',
+      headers: {'content-type' : 'application/json', 'accept' : 'application/json'},
+      body : JSON.stringify(toy)
+    })
+    .then(function(response) {return response.json()})
+    .then(function(newToy) {createToy(newToy)})
   }
 
   const createToy = (toy) => {
@@ -33,6 +45,20 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  newToyForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    let name = e.target.querySelector('input').value;
+    let imageUrl = e.target.querySelectorAll('input')[1].value;
+    console.log(name, imageUrl);
+
+    if  (name.length > 0 && imageUrl.length > 0 ){
+      let newToy = {'name' : name, 'image' : imageUrl, 'likes' : 0};
+      postToy(newToy);
+      e.target.querySelector('input').value = null;
+      e.target.querySelectorAll('input')[1].value = null;
+    }
+  })
 
   fetchAllToys();
 
