8cc685c0769d34aa537897a4fd8217c1101f67cf
mjester93 <m.jester.93@gmail.com>
2020-07-16 16:36:05 -0400

    adding like feature

--- src/index.js
+++ src/index.js
@@ -24,15 +24,25 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(function(newToy) {createToy(newToy)})
   }
 
+  const updateLike = (toyId, likes) => {
+    fetch(`${toyUrl}/${toyId}`, {
+      method: 'PATCH',
+      headers: {'content-type' : 'application/json', 'accept' : 'application/json'},
+      body : JSON.stringify({"likes": likes})
+    })
+    .then(function(response) {return response.json()})
+    .then(function(newToy) {return newToy})
+  }
+
   const createToy = (toy) => {
     let div = document.createElement('div');
     div.className = "card"
+    div.dataset.toyId = toy.id;
     div.innerHTML = `
     <h2>${toy.name}</h2>
     <img src=${toy.image} class="toy-avatar" />
-    <p>${toy.likes} Likes </p>
-    <button class="like-btn">Like <3</button>
-    `
+    <p><span>${toy.likes}</span> Likes </p>
+    <button class="like-btn">Like <3</button>`
     toyCollection.appendChild(div);
   }
 
@@ -45,6 +55,18 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  toyCollection.addEventListener("click", (e) => {
+    if (e.target.className === "like-btn") {
+      let currentToy = e.target.parentNode;
+      let currentToyId = currentToy.dataset.toyId;
+      let currentLikes = currentToy.querySelector('p > span');
+      currentLikes.innerText = parseInt(currentLikes.innerText, 10) + 1
+      currentLikes = currentToy.querySelector('p > span').innerText;
+
+      updateLike(currentToyId, currentLikes);
+    }
+  })
 
   newToyForm.addEventListener("submit", (e) =>{
     e.preventDefault();
