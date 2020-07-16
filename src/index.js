30634ddb026432e4a130cbe8e7347497bffd7790
mjester93 <m.jester.93@gmail.com>
2020-07-16 15:10:44 -0400

    creating toy divs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.querySelector("#toy-collection");
+  const toyUrl = "http://localhost:3000/toys";
+
+  const fetchAllToys = () => {
+    fetch(toyUrl)
+    .then(function(response) {return response.json()})
+    .then(function(toys) {toys.forEach(toy => createToy(toy))})
+  }
+
+  const createToy = (toy) => {
+    let div = document.createElement('div');
+    div.className = "card"
+    div.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes} Likes </p>
+    <button class="like-btn">Like <3</button>
+    `
+    toyCollection.appendChild(div);
+  }
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetchAllToys();
+
 });
